03dbdf70dac7cef9d9e0a6c0413665161996ce9c
Andrey Titov <n3kto.1@yandex.ru>
2022-01-14 22:54:49 +0300

    feat: добавлен адаптив для popup и реалихована функциональность на js смены profile__title и profile__subtitle

--- pages/script.js
+++ pages/script.js
@@ -1,35 +1,29 @@
 const profileOpenPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
 const popupCloseButton = document.querySelector('.popup__close-icon');
 const popupSubmitButton = document.querySelector('.popup__submit-button');
 
-console.log(profileTitle.textContent);
-console.log(profileSubtitle.textContent);
-
-function OpenPopup () {
+function OpenPopup() {
     console.log("Открыть");
     popup.classList.add('popup__open');
 }
 
-function ClosePopup () {
+function ClosePopup() {
     console.log("Закрыть");
     popup.classList.remove('popup__open');
 }
 
-function SubmitButton () {
+function SubmitButton() {
     console.log("Сохранить");
-    let popupFieldName = document.getElementsByClassName('.popup__field-name').textContent;
-    console.log(popupFieldName.textContent);
-    // profileTitle.textContent = '' + popupFieldName.textContent;
-    let popupFieldAbout = document.querySelector('.popup__field-about');
-    console.log(popupFieldAbout.textContent);
-    // profileSubtitle.textContent = '' + popupFieldAbout.textContent;
-    
-    
-    
-    // ClosePopup();
+    let popupFieldName = document.querySelector('.popup__field-name').value;
+    console.log(popupFieldName);
+    document.querySelector('.profile__title').textContent = popupFieldName;
+
+    let popupFieldAbout = document.querySelector('.popup__field-about').value;
+    console.log(popupFieldAbout);
+    document.querySelector('.profile__subtitle').textContent = popupFieldAbout;
+
+    ClosePopup();
 }
 
 profileOpenPopupButton.addEventListener('click', OpenPopup);
