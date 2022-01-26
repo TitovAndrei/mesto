5b09eb7b0e081c3d6c6cc12796bb76af20055e02
Andrey Titov <n3kto.1@yandex.ru>
2022-01-26 21:06:25 +0300

    feature/script.js: Добавил обработчик кнопки удаления и функцию добавления карточек на основе данных исходного массива в ДОМ страницы

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ let fieldJob = document.querySelector('.profile__subtitle');
 let profileOpenPopupButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-icon');
 
+
 const initialCards = [
     {
       name: 'Архыз',
@@ -34,23 +35,67 @@ const initialCards = [
     }
   ]; 
 
+initialCards.forEach(function (item){
+    
+    const element = document.querySelector('#element').content;
+    const elements = document.querySelector('.elements');
+    // клонируем содержимое тега template
+    const newElement = element.querySelector('.element').cloneNode(true);
+    // наполняем содержимым
+    newElement.querySelector('.element__mask-group').src = item.link;
+    newElement.querySelector('.element__title').textContent = item.name;
+    
+    // отображаем на странице
+    elements.append(newElement);
+    
+});
+
+let buttomDelElement = document.querySelector('.element__del');
+
+
+// function addElement(artistValue, titleValue) {
+//     const element = document.querySelector('#element').content;
+//     const elements = document.querySelector('.elements');
+//     // наполняем содержимым
+//     newElement.querySelector('.element__mask-group').src = item.link;
+//     newElement.querySelector('.element__title').textContent = item.name;
+    
+//     // отображаем на странице
+//     elements.unshift(newElement);
+       
+//   }
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = fieldName.textContent;
     jobInput.value = fieldJob.textContent;
-}
+};
+
 function closePopup() {
     popup.classList.remove('popup_opened');
-}
+};
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
     fieldName.textContent = nameInput.value;
     fieldJob.textContent = jobInput.value;
     closePopup();
-}
+};
 
+function delElement () {
+    const listItem = buttomDelElement.closest('.element');
+    listItem.remove(buttomDelElement = document.querySelector('.element__del'));
+    
+};
+
+
+
+// songElement.querySelector('.song__like').addEventListener('click', function (evt) {
+//     evt.target.classList.toggle('song__like_active');
+//   });
 profileOpenPopupButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+buttomDelElement.addEventListener('click', delElement);
 profileEditingForm.addEventListener('submit', formSubmitHandler);
 
