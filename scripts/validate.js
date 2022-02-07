c83a0f71ae09741a67726ee9aaa43fde747b992d
Andrey Titov <n3kto.1@yandex.ru>
2022-02-07 19:17:03 +0300

    feature/validate.js: сделан рефакторинг кода, enableValidation принимает в качестве аргумента объект реализована функциональность ПР6

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,55 +1,72 @@
 // Находим все формы на странице
-const enableValidation = () => {
-    const formList = Array.from(document.querySelectorAll('.popup__form'));
+const enableValidation = (obj) => {
+    const formList = Array.from(document.querySelectorAll(obj.formSelector));
     formList.forEach((formElement) => {
-        setEventListeners(formElement);
+        inputElement = obj.fieldSelector;
+        setEventListeners(obj, formElement, inputElement);
     });
 };
 
 // Находим все поля на странице
-const setEventListeners = (formElement) => {
-    const fieldList = Array.from(formElement.querySelectorAll('.popup__field'));
+const setEventListeners = (obj, formElement, inputElement) => {
+    const fieldList = Array.from(formElement.querySelectorAll(inputElement));
     fieldList.forEach((fieldElement) => {
         fieldElement.addEventListener('input', () => {
-            isValidate(formElement, fieldElement)
+            isValidate(obj, formElement, fieldElement)
         });
     });
 };
 
 //проверяем на валидность
-const isValidate = (formElement, fieldElement) => {
+const isValidate = (obj, formElement, fieldElement) => {
     if (!fieldElement.validity.valid) {
-        markInputError(formElement, fieldElement, fieldElement.validationMessage);
+        markInputError(obj, formElement, fieldElement, fieldElement.validationMessage);
     } else {
-        hideInputError(formElement, fieldElement);
+        hideInputError(obj, formElement, fieldElement);
     }
 };
 
-// Функция, которая подсвечивает поле не прошедшее валидацию и скрывает кнопку
-const markInputError = (element, fieldElement, errorMessage) => {
-    const popupInput = fieldElement.closest('.popup__input');
-    const errorElement = popupInput.querySelector('.popup__field_error');
+// подсвечиваем поле не прошедшее валидацию
+const markInputError = (obj, formElement, fieldElement, errorMessage) => {
+    const popupInput = fieldElement.closest(obj.inputSelector);
+    const errorElement = popupInput.querySelector(obj.errorSelector);
     errorElement.textContent = errorMessage;
-    const button = element.querySelector('.popup__submit-button');
-    if(button) {
-        button.classList.add('popup__submit-button_disabled');
-    } 
-    fieldElement.classList.add('popup__field_type_error');
+    fieldElement.classList.add(obj.fieldErrorClass);
+    changingButtonState(obj, formElement, false);
 };
 
-// Функция, которая показывает поля прошедние валидацию и показывает кнопку
-const hideInputError = (element, fieldElement) => {
-    const popupInput = fieldElement.closest('.popup__input');
-    const errorElement = popupInput.querySelector('.popup__field_error');
+// определяем поля прошедние валидацию и показывает кнопку
+const hideInputError = (obj, formElement, fieldElement) => {
+    const popupInput = fieldElement.closest(obj.inputSelector);
+    const errorElement = popupInput.querySelector(obj.errorSelector);
     errorElement.textContent = null;
-    fieldElement.classList.remove('popup__field_type_error');
-    const valid = element.querySelector('.popup__field_type_error');
-    const button = element.querySelector('.popup__submit-button_disabled');
-    if(button && !valid) {
-        button.classList.remove('popup__submit-button_disabled');
-    }  
-    
+    fieldElement.classList.remove(obj.fieldErrorClass);
+    changingButtonState(obj, formElement, true);
+
 };
+
+// функция активации и деактевации кнопки 
+function changingButtonState(obj, formElement, boolean) {
+    const button = formElement.querySelector(obj.submitButtonSelector);
+    const valid = formElement.querySelector(`.${obj.fieldErrorClass}`);
+
+    console.log(obj.inactiveButtonClass);
+    console.log(button);
+    if (button && boolean === true && !valid) {
+        button.classList.remove(obj.inactiveButtonClass);
+    }
+    else if (button && boolean === false && valid) {
+        button.classList.add(obj.inactiveButtonClass);
+    }
+}
 
 // Вызовем функцию
-enableValidation(); 
+enableValidation({
+    inputSelector: '.popup__input',
+    formSelector: '.popup__form',
+    fieldSelector: '.popup__field',
+    submitButtonSelector: '.popup__submit-button',
+    errorSelector: '.popup__field_error',
+    inactiveButtonClass: 'popup__submit-button_disabled',
+    fieldErrorClass: 'popup__field_type_error'
+});
